3fb9b8c38e7f28921a96abda6be46fe4b5590f60
Yulia Vladimirov <julia.vladimirov@gmail.com>
2024-03-01 20:24:52 +0100

    HWs_last update

---
+++ Alg_Homework_2024.02.22/src/main/java/org/example/TowerRecursion.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class TowerRecursion {
+    public static void main(String[] args) {
+        int num = 3;
+        towerOfHanoiZero(num, 'A', 'C', 'B');
+    }
+
+    public static void towerOfHanoi(int numOfWheels, char from, char to, char help) {
+        if (numOfWheels == 1) {
+            System.out.println("Take disk from tower " + from + " to tower " + to);
+            return;
+        }
+        towerOfHanoi(numOfWheels - 1, from, help, to);
+        System.out.println("Take disk from tower " + from + " to tower " + to);
+        towerOfHanoi(numOfWheels - 1, help, to, from);
+    }
+    public static void towerOfHanoiZero(int numOfWheels, char from, char to, char help) {
+        if (numOfWheels == 0) {
+            return;
+        }
+//        action 1
+        towerOfHanoi(numOfWheels - 1, from, help, to);
+//        action 2
+        System.out.println("Take disk from tower " + from + " to tower " + to);
+        towerOfHanoi(numOfWheels - 1, help, to, from);
+    }
+}
